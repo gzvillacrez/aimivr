e3a948bc2742edba588ad12d2212d79d4853ff82
gonzalo <gonzalovillacrez@gmail.com>
2017-04-19 21:44:30 -0500

    modificado

---
+++ Pagina2.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Proyecto aimi </title>
+<link rel="stylesheet" href="/css/Estilos.css">
+  </head>
+  <body>
+    <p>
+      hola mundo
+    </p>
+      <a href="index.php"> Pagina 1 </a>
+      <p>hola mundoo
+      </p>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype.html>
+<html lang="es">
+<head>
+<title>   " Proyecto Aimi " </title>
+
+<link rel="stylesheet" href="/css/Estilos.css">
+
+</head>
+<body>
+  <h1> hola mundo </h1>
+  <p> hola esto  mi primera pagina  web  </p>
+  <p> la pagina  instrumento de   desarrollo </p>
+  <hr>
+  <h2>" full stack " </h2>
+  <h2 class="colores" </h2>
+  <h3> Desarrollando </h3>
+  <h4> esto es una Prueba</h4>
+  <p class="principal"> hola mundo
+  </p>
+  <a href="Pagina2.php"> Pagina 2 </a>
+
+<img src="imagenes/aimi.png" alt="aimi" />
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+</html>
